5e1ae9e37cef4c674491d621dc58d51da37b2a77
Federico Carbone <fedebone00@hotmail.it>
2021-07-27 00:32:42 +0200

    Revert "working sha256d implementation"
    This reverts commit 79f7271c56c8f853444ade59929cbac84d38bd6c.

--- sha256d_final.srcs/sources_1/new/sha256.vhd
+++ sha256d_final.srcs/sources_1/new/sha256.vhd
@@ -47,7 +47,7 @@ entity sha256 is
 end sha256;
 
 architecture Behavioral of sha256 is       
-    type state is (waiting, preprocess, words_ass, hash_calc_step, hv_calc, finished, reset);
+    type state is (waiting, preprocess, words_ass, t_1_2_calc, hash_calc_step, hv_calc, finished, reset);
     signal currentstate, nextstate: state;
     constant blocks_count: integer := (SIZE+65)/512 + 1;
     
@@ -81,17 +81,15 @@ begin
     
     w_generator:
     for i in 0 to 63 generate
+        W <= W;
         FIRST_15: if i<16 generate
-            W(i) <= message_blocks(message_block_counter_int_val)((16-i)*32-1 downto (15-i)*32) when message_block_counter_int_val>-1 else (others=>'U');
+            W(i) <= message_blocks(message_block_counter_int_val)((16-i)*32-1 downto (15-i)*32) when message_block_counter_int_val>-1;
         end generate FIRST_15;
         
         NEXT_16: if i>=16 generate
-            W(i) <= std_logic_vector(unsigned(small_s1(W(i-2))) + unsigned(W(i-7)) + unsigned(small_s0(W(i-15))) + unsigned(W(i-16))) when message_block_counter_int_val>-1 else (others=>'U');
+            W(i) <= std_logic_vector(unsigned(small_s1(W(i-2))) + unsigned(W(i-7)) + unsigned(small_s0(W(i-15))) + unsigned(W(i-16))) when message_block_counter_int_val>-1;
         end generate NEXT_16;
     end generate w_generator;
-    
-    T1 <= unsigned(words(7))+unsigned(BIG_S1(words(4)))+unsigned(Ch(words(4), words(5), words(6)))+unsigned(K(hash_step_counter_int_val))+unsigned(W(hash_step_counter_int_val)) when hash_step_counter_int_val>-1 and hash_step_counter_int_val<64 else (others=>'U');
-    T2 <= unsigned(BIG_S0(words(0)))+unsigned(Maj(words(0), words(1), words(2)));
 
     message_block_counter_int_val <= to_integer(message_block_counter_val);
     message_block_counter_init <= '1' when currentstate=preprocess else '0';
@@ -105,22 +103,29 @@ begin
                                     
     hash_step_counter_int_val <= to_integer(hash_step_counter_val);
     hash_step_counter_init <= '1' when currentstate=preprocess or currentstate=hv_calc else '0';
-    hash_step_counter_enable <= '1' when currentstate=hash_calc_step else '0';
-    hash_step_counter: entity work.counter generic map (SIZE=>7, TERMINAL=>63)
+    hash_step_counter_enable <= '1' when currentstate=t_1_2_calc else '0';
+    hash_step_counter: entity work.counter generic map (SIZE=>7, TERMINAL=>64)
         port map (  clock=>clk, reset=>rst,
                     counter_init=>hash_step_counter_init,
                     counter_enable=>hash_step_counter_enable,
                     counter_tc=>hash_step_counter_tc,
                     counter_val=>hash_step_counter_val);
     
-    process(clk, currentstate, T1, T2) is
+    process(currentstate, message_block_counter_int_val) is
     begin
-        if rising_edge(clk) then
+        hv <= hv;
+        words <= words;
+        T1 <= T1;
+        T2 <= T2;
+--        if rising_edge(clk) then
             case currentstate is
                 when preprocess =>
                     hv <= init_hv;
                 when words_ass =>
                     words <= hv;
+                when t_1_2_calc =>
+                    T1 <= unsigned(words(7))+unsigned(BIG_S1(words(4)))+unsigned(Ch(words(4), words(5), words(6)))+unsigned(K(hash_step_counter_int_val))+unsigned(W(hash_step_counter_int_val));
+                    T2 <= unsigned(BIG_S0(words(0)))+unsigned(Maj(words(0), words(1), words(2)));
                 when hash_calc_step =>
                     words(7) <= words(6);
                     words(6) <= words(5);
@@ -136,7 +141,7 @@ begin
                     end loop;
                 when others =>
             end case;
-        end if;
+--        end if;
     end process;
     
     process(clk, rst) is
@@ -148,7 +153,7 @@ begin
         end if;
     end process;
     
-    process(clk, rst, start, currentstate, hash_step_counter_tc, message_block_counter_tc) is
+    process(clk, rst, start, hash_step_counter_tc, message_block_counter_tc) is
     begin
         case currentstate is
             when waiting =>
@@ -158,12 +163,13 @@ begin
                     nextstate <= waiting;
                 end if;
             when preprocess => nextstate <= words_ass;
-            when words_ass => nextstate <= hash_calc_step;
+            when words_ass => nextstate <= t_1_2_calc;
+            when t_1_2_calc => nextstate <= hash_calc_step;
             when hash_calc_step =>
                 if hash_step_counter_tc='1' then
                     nextstate <= hv_calc;
                 else
-                    nextstate <= hash_calc_step;
+                    nextstate <= t_1_2_calc;
                 end if;
             when hv_calc =>
                 if message_block_counter_tc='1' then
