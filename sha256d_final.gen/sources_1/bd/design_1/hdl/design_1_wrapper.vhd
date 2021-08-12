c03a688bdeb385419d09bd957d6200ac5343adcc
Federico Carbone <fedebone00@hotmail.it>
2021-08-12 10:31:36 +0200

    implemented block design for hardware export, sha256d logic moved to sha256d_axi_ip ip block, modify it throught sha256d_final.tmp/sha256d_axi_ip_v1_0_project

--- sha256d_final.gen/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
+++ sha256d_final.gen/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2020.2 (win64) Build 3064766 Wed Nov 18 09:12:45 MST 2020
---Date        : Mon Jul 12 19:48:14 2021
+--Date        : Wed Aug 11 17:13:01 2021
 --Host        : LAPTOP-S28JR86F running 64-bit major release  (build 9200)
 --Command     : generate_target design_1_wrapper.bd
 --Design      : design_1_wrapper
@@ -33,17 +33,13 @@ entity design_1_wrapper is
     FIXED_IO_mio : inout STD_LOGIC_VECTOR ( 53 downto 0 );
     FIXED_IO_ps_clk : inout STD_LOGIC;
     FIXED_IO_ps_porb : inout STD_LOGIC;
-    FIXED_IO_ps_srstb : inout STD_LOGIC;
-    btns_5bits_tri_i : in STD_LOGIC_VECTOR ( 4 downto 0 );
-    leds_8bits_tri_o : out STD_LOGIC_VECTOR ( 7 downto 0 );
-    sws_8bits_tri_i : in STD_LOGIC_VECTOR ( 7 downto 0 )
+    FIXED_IO_ps_srstb : inout STD_LOGIC
   );
 end design_1_wrapper;
 
 architecture STRUCTURE of design_1_wrapper is
   component design_1 is
   port (
-    sws_8bits_tri_i : in STD_LOGIC_VECTOR ( 7 downto 0 );
     DDR_cas_n : inout STD_LOGIC;
     DDR_cke : inout STD_LOGIC;
     DDR_ck_n : inout STD_LOGIC;
@@ -64,9 +60,7 @@ architecture STRUCTURE of design_1_wrapper is
     FIXED_IO_ddr_vrp : inout STD_LOGIC;
     FIXED_IO_ps_srstb : inout STD_LOGIC;
     FIXED_IO_ps_clk : inout STD_LOGIC;
-    FIXED_IO_ps_porb : inout STD_LOGIC;
-    btns_5bits_tri_i : in STD_LOGIC_VECTOR ( 4 downto 0 );
-    leds_8bits_tri_o : out STD_LOGIC_VECTOR ( 7 downto 0 )
+    FIXED_IO_ps_porb : inout STD_LOGIC
   );
   end component design_1;
 begin
@@ -92,9 +86,6 @@ design_1_i: component design_1
       FIXED_IO_mio(53 downto 0) => FIXED_IO_mio(53 downto 0),
       FIXED_IO_ps_clk => FIXED_IO_ps_clk,
       FIXED_IO_ps_porb => FIXED_IO_ps_porb,
-      FIXED_IO_ps_srstb => FIXED_IO_ps_srstb,
-      btns_5bits_tri_i(4 downto 0) => btns_5bits_tri_i(4 downto 0),
-      leds_8bits_tri_o(7 downto 0) => leds_8bits_tri_o(7 downto 0),
-      sws_8bits_tri_i(7 downto 0) => sws_8bits_tri_i(7 downto 0)
+      FIXED_IO_ps_srstb => FIXED_IO_ps_srstb
     );
 end STRUCTURE;
---
+++ vitis_workspace/design_1_wrapper/platform.tcl
@@ -0,0 +1,28 @@
+# 
+# Usage: To re-create this platform project launch xsct with below options.
+# xsct D:\Uni\RL\sha256d_zedboard\vitis_workspace\design_1_wrapper\platform.tcl
+# 
+# OR launch xsct and run below command.
+# source D:\Uni\RL\sha256d_zedboard\vitis_workspace\design_1_wrapper\platform.tcl
+# 
+# To create the platform in a different location, modify the -out option of "platform create" command.
+# -out option specifies the output directory of the platform project.
+
+platform create -name {design_1_wrapper}\
+-hw {D:\Uni\RL\sha256d_zedboard\design_1_wrapper.xsa}\
+-no-boot-bsp -fsbl-target {psu_cortexa53_0} -out {D:/Uni/RL/sha256d_zedboard/vitis_workspace}
+
+platform write
+domain create -name {standalone_ps7_cortexa9_0} -display-name {standalone_ps7_cortexa9_0} -os {standalone} -proc {ps7_cortexa9_0} -runtime {cpp} -arch {32-bit} -support-app {hello_world}
+platform generate -domains 
+platform write
+platform generate -quick
+bsp reload
+bsp setlib -name xilpm -ver 3.2
+bsp write
+bsp reload
+catch {bsp regenerate}
+bsp removelib -name xilpm
+bsp write
+bsp reload
+catch {bsp regenerate}
